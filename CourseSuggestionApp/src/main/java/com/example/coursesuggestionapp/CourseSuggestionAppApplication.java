bd3996d53fdd8e419b41aa52bc6bab4f404d4280
Gorgi Nikoloski <gorgi1nikoloski@gmail.com>
2025-04-04 21:20:26 +0200

    first commit

---
+++ CourseSuggestionApp/src/main/java/com/example/coursesuggestionapp/CourseSuggestionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursesuggestionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSuggestionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSuggestionAppApplication.class, args);
+    }
+
+}
---
+++ CourseSuggestionApp/src/test/java/com/example/coursesuggestionapp/CourseSuggestionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursesuggestionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSuggestionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
