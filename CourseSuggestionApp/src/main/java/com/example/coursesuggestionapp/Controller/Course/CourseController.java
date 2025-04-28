811707a27c8c5f8472e3eff1e947aed37f2fde9a
Petar-Mladenovski <pm.working.email@gmail.com>
2025-04-28 13:35:39 +0200

    Created CourseController with endpoints for fetching mandatory courses by majorId and semesterNo params, and elective courses by majorId and levelNo params. Also created StudyMajorController with endpoint for fetching majorId by userId param

---
+++ CourseSuggestionApp/src/main/java/com/example/coursesuggestionapp/Controller/Course/CourseController.java
@@ -0,0 +1,28 @@
+package com.example.coursesuggestionapp.Controller.Course;
+
+import com.example.coursesuggestionapp.Models.DTO.CourseDTO;
+import com.example.coursesuggestionapp.Service.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping("/by-major/{majorId}/semester/{semesterNo}")
+    public List<CourseDTO> getMandatoryCoursesByMajorIdAndSemesterNo(@PathVariable Long majorId, @PathVariable Integer semesterNo) {
+        return courseService.getCoursesByMajorIdAndSemesterNo(majorId, semesterNo);
+    }
+
+    @GetMapping("/by-major/{majorId}/elective/{levelNo}")
+    public List<CourseDTO> getElectiveCoursesByMajorIdAndLevelNo(@PathVariable Long majorId, @PathVariable Integer levelNo) {
+        return courseService.getElectiveCoursesByMajorIdAndLevelNo(majorId, levelNo);
+    }
+}
