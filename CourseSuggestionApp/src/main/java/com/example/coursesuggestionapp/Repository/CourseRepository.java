f70355f53ff8cfa11f36b8c757bb37c59b7e6883
Petar-Mladenovski <pm.working.email@gmail.com>
2025-04-28 13:47:46 +0200

    Updated CourseRepository with inserting queries for fetching course data

--- CourseSuggestionApp/src/main/java/com/example/coursesuggestionapp/Repository/CourseRepository.java
+++ CourseSuggestionApp/src/main/java/com/example/coursesuggestionapp/Repository/CourseRepository.java
@@ -2,7 +2,9 @@ package com.example.coursesuggestionapp.Repository;
 
 import com.example.coursesuggestionapp.Models.Entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
@@ -10,4 +12,26 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     Optional<Course> findByCourseCode(String courseCode);
 
     Optional<Course> findByCourseName(String courseName);
+
+    @Query("""
+        SELECT c
+        FROM MandatoryCourse mc
+        JOIN Course c ON mc.courseId = c.courseId
+        WHERE mc.majorId = :majorId
+        AND mc.semesterNo = :semesterNo
+        """)
+    List<Course> findCoursesByMajorIdAndSemesterNo(@Param("majorId") Long majorId, @Param("semesterNo") Integer semesterNo);
+
+    @Query("""
+        SELECT c
+        FROM Course c
+        WHERE c.courseId NOT IN (
+            SELECT mc.courseId
+            FROM MandatoryCourse mc
+            WHERE mc.majorId = :majorId
+        )
+        AND c.courseLevel = :levelNo
+        """)
+    List<Course> findElectiveCoursesByMajorIdAndLevelNo(@Param("majorId") Long majorId, @Param("levelNo") Integer levelNo);
+
 }
